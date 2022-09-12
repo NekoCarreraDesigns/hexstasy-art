d948575e848041120db28b4e5bd0039d0736f76e
NekoCarreraDesigns <denvermaas@gmail.com>
2022-09-12 14:40:42 -0600

    added Navabr to App.tsx

--- hexstasy-art/src/App.tsx
+++ hexstasy-art/src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
     <div className='App'>
-      <h1>Hexstasy Art</h1>
+      <Navbar />
     </div>
   );
 }
