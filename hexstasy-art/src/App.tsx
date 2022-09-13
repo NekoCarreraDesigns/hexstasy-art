0345b6987fd7f42d815bca4c2a9be60da8ab6e1a
NekoCarreraDesigns <denvermaas@gmail.com>
2022-09-13 15:45:32 -0600

    added code for routes and provider

--- hexstasy-art/src/App.tsx
+++ hexstasy-art/src/App.tsx
@@ -1,11 +1,27 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Store } from "./pages/Store";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext";
+
 import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
-    <div className='App'>
-      <Navbar />
-    </div>
+    <>
+      <ShoppingCartProvider>
+        <Navbar />
+        <Container className='mb-5'>
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/store' element={<Store />}></Route>
+            <Route path='/about' element={<About />}></Route>
+          </Routes>
+        </Container>
+      </ShoppingCartProvider>
+    </>
   );
 }
 
