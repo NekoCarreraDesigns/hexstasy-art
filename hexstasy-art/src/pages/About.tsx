3a7caa0f0fc282884cca7e654195ec1903e0871b
NekoCarreraDesigns <denvermaas@gmail.com>
2022-09-13 15:43:29 -0600

    added code to about page

---
+++ hexstasy-art/src/pages/About.tsx
@@ -0,0 +1,15 @@
+import { Card } from "react-bootstrap";
+
+export function About() {
+  return (
+    <Card className='d-flex justify-content-center'>
+      <Card.Title>About</Card.Title>
+      <p>
+        Candace Heckstall is a multimedia artist, travel blogger, podcaster, and
+        believer in living life to the fullest. inspired by nature and her
+        travels to various places. she uses bold bright colors, and draws deeply
+        from the emotions felt from the experiences she has had.
+      </p>
+    </Card>
+  );
+}
