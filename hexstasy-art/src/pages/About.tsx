6ade0bb52abc4cdd60b9c85287c5de7fcf8966a8
NekoCarreraDesigns <denvermaas@gmail.com>
2022-09-14 15:08:04 -0600

    added image to card

--- hexstasy-art/src/pages/About.tsx
+++ hexstasy-art/src/pages/About.tsx
@@ -1,4 +1,5 @@
 import { Card } from "react-bootstrap";
+import "./About.css";
 
 export function About() {
   return (
@@ -10,6 +11,10 @@ export function About() {
         travels to various places. she uses bold bright colors, and draws deeply
         from the emotions felt from the experiences she has had.
       </p>
+      <img
+        alt='Candace-holding-a-cat-named-spike'
+        src='./images/Candace_with_Spike.jpg'
+        className='candace-picture'></img>
     </Card>
   );
 }
